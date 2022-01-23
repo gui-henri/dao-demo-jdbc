3176746cb448c27365463f4bdd33cfa5d7933b55
Guilherme Henrique <guihenrique.dev@gmail.com>
2022-01-23 14:57:21 -0300

    Department entity class

--- src/com/application/Main.java
+++ src/com/application/Main.java
@@ -1,8 +1,11 @@
 package com.application;
 
+import model.entities.Department;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Department dp = new Department(1, "Books");
+        System.out.println(dp);
     }
 }
