90702f7ae6f6adabcf89e5a35b5008e0cde3f8a7
Guilherme Henrique <guihenrique.dev@gmail.com>
2022-01-23 23:13:05 -0300

    Seller entity Class

--- src/com/application/Main.java
+++ src/com/application/Main.java
@@ -1,11 +1,16 @@
 package com.application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
         Department dp = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dp);
         System.out.println(dp);
+        System.out.println(seller);
     }
 }
